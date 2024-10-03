aceacfee286b41b7cf6822bd1dc11a9ab8bd0a0c
Logan Conley <logan.conley123@gmail.com>
2024-10-03 17:01:10 -0400

    - Added InventoryController and LoansController - Added empty index views for Inventory and Loans - Changed _layout.cshtml so that the Inventory and Active Loans buttons referenced the InventoyController and LoansController

---
+++ Controllers/LoansController.cs
@@ -0,0 +1,26 @@
+﻿using AssetSentry.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AssetSentry.Controllers
+{
+    public class LoansController : Controller
+    {
+        private readonly ILogger<LoansController> _logger;
+
+        public LoansController(ILogger<LoansController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
