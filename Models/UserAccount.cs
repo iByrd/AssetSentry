855e626b7ab094e453d958217c2590ffb420add0
Logan Conley <logan.conley123@gmail.com>
2025-01-19 14:37:00 -0500

    Added UserAccount to models folder

---
+++ Models/UserAccount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace AssetSentry.Models
+{
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(Email), IsUnique = true)]
+    public class UserAccount
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        [MaxLength(50, ErrorMessage = "Maximum of 50 characters allowed")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [MaxLength(50, ErrorMessage = "Maximum of 50 characters allowed")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [MaxLength(100, ErrorMessage = "Maximum of 50 characters allowed")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
