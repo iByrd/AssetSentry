f0e15eeafe091346be3eb17c0b30986ca5fd6f19
Logan Conley <logan.conley123@gmail.com>
2025-01-20 12:07:17 -0500

    Added a UserAccounts table to the database by adding it to AssetSentryContext, creating a new Migration, and updating the database

---
+++ Migrations/20250120165743_UserAccounts.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetSentry.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserAccounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Loans",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.CreateTable(
+                name: "UserAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAccounts_Email",
+                table: "UserAccounts",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAccounts");
+
+            migrationBuilder.InsertData(
+                table: "Loans",
+                columns: new[] { "Id", "DeviceId", "EndDate", "IsActive", "StartDate", "Student" },
+                values: new object[] { 1, 1, new DateTime(2019, 6, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), true, new DateTime(2019, 6, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "David Miller" });
+        }
+    }
+}
--- Models/AssetSentryContext.cs
+++ Models/AssetSentryContext.cs
@@ -12,6 +12,8 @@ namespace AssetSentry.Models
 
         public DbSet<Loan> Loans { get; set; }
 
+        public DbSet<UserAccount> UserAccounts { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
