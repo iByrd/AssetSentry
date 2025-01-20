972af61181c8f75379801490bf22ac5075754879
Logan Conley <logan.conley123@gmail.com>
2025-01-20 12:56:02 -0500

    Added a RegistrationViewModel with mostly the same attributes as the UserAccount and removed the Password datatype from the UserAccount Password attribute. The UserAccount doesn't need the password datatype because users will first create their password in registration.

--- Models/RegistrationViewModel.cs
+++ Models/RegistrationViewModel.cs
@@ -1,15 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-using System.Runtime.CompilerServices;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace AssetSentry.Models
 {
-    [Index(nameof(Email), IsUnique = true)]
-    [Index(nameof(Email), IsUnique = true)]
-    public class UserAccount
+    public class RegistrationViewModel
     {
-        public int Id { get; set; }
-
         [Required(ErrorMessage = "First name is required.")]
         [MaxLength(50, ErrorMessage = "Maximum of 50 characters allowed.")]
         public string FirstName { get; set; }
@@ -19,10 +13,16 @@ namespace AssetSentry.Models
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Email is required.")]
-        [MaxLength(100, ErrorMessage = "Maximum of 50 characters allowed.")]
+        [MaxLength(100, ErrorMessage = "Maximum of 100 characters allowed.")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please Enter Valid Email.")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Confirmation password does not match password.")]
+        [MinLength(5), ErrorMessage = "Minimum of 5 characters required."]
+        public string ConfirmPassword { get; set; }
     }
 }
