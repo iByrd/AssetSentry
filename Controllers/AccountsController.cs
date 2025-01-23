17e5dec587fc95593ddfcea4641f0e11e290f60d
Logan Conley <logan.conley123@gmail.com>
2025-01-23 08:04:22 -0500

    Adding an AccountController to manage AccountList, Registration, Login, Logout, and SecurePage user interactions

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,103 @@
+﻿using AssetSentry.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace AssetSentry.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly AssetSentryContext _context;
+
+        public AccountsController(AssetSentryContext context) => _context = context;
+
+        public IActionResult AccountList()
+        {
+            return View(_context.UserAccounts.ToList());
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registration(RegistrationViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                UserAccount account = new UserAccount();
+                account.FirstName = model.FirstName;
+                account.LastName = model.LastName;
+                account.Email = model.Email;
+                account.Password = model.Password;
+
+                try
+                {
+                    _context.UserAccounts.Add(account);
+                    _context.SaveChanges();
+
+                    ModelState.Clear();
+                    ViewBag.Message = $"{account.FirstName} {account.LastName} registered successfully. Please login.";
+                }
+                catch (DbUpdateException ex)
+                {
+                    ModelState.AddModelError("", "Please enter unique Email and Password.");
+                    return View(model);
+                }
+                return View();
+            }
+            return View(model);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _context.UserAccounts.Where(x => x.Email == model.Email && x.Password == model.Password).FirstOrDefault();
+                if (user != null)
+                {
+                    //Success, create cookie
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, user.Email),
+                        new Claim("Name", user.FirstName),
+                        new Claim(ClaimTypes.Role, "User")
+                    };
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+                    return RedirectToAction("SecurePage");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email or Password is not correct.");
+                }
+            }
+            return View();
+        }
+
+        public IActionResult LogOut()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        public IActionResult SecurePage()
+        {
+            ViewBag.Name = HttpContext.User.Identity.Name;
+            return View();
+        }
+    }
+}
